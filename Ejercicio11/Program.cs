ea45dfcabee3c6c1abd3aa63baecc183db54bb34
Jeriko34 <goldplayer1207@gmail.com>
2024-10-24 09:51:15 -0500

    Segundo com

---
+++ Ejercicio11/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] numero = new int [1];
+Random n = new Random();
+
+Console.WriteLine("Ingrese la cantidad de valores que gustes");
+int num= int.Parse(Console.ReadLine());
+
+for (int x = 0; x > num; num++)
+{
+    numero[x] = n.Next(50, 90); 
+}
+Console.WriteLine("¿Le gusta los números pares o impares? (p/i)");
+string tipo = Console.ReadLine();
+
+if (numero )
